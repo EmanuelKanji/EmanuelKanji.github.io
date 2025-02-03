f757f21722d1f97637e5f764827bbb96e39d5b73
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-02 20:54:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config(); // Cargar variables de entorno
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const contactRoutes = require('./backend/routes/contactRoutes');  // Suponiendo que este es tu archivo de rutas
+const contactRoutes = require('./backend/routes/contactRoutes'); 
 
 const app = express();
 
@@ -11,14 +13,15 @@ app.use(cors());
 // Middleware para parsear datos en formato JSON
 app.use(express.json());
 
-// Conexión a MongoDB Atlas (asegúrate de reemplazar el <url> con la URL de tu base de datos)
-mongoose.connect('<url>', { useNewUrlParser: true, useUnifiedTopology: true })
+// Conexión a MongoDB Atlas usando variables de entorno
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a la base de datos MongoDB Atlas'))
   .catch((err) => console.log('Error al conectar a la base de datos:', err));
 
 // Ruta para las solicitudes de contacto
 app.use('/api/contacto', contactRoutes);
 
-app.listen(5000, () => {
-    console.log('Servidor corriendo en http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
