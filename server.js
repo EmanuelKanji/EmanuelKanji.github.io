f398f1f054839c8f8c5c2ffeb4650e6715665abb
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-02 16:34:50 -0800

    update

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
+// Importar variables de entorno
 require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const config = require('./backend/config/config');
-require('./backend/config/db');  // Conectar a la base de datos de MongoDB
 
+// Importar express, para crear el servidor
+const express = require('express');
+
+// Importar mongoose, para interactuar con la base de datos de MongoDB
+const mongoose = require('mongoose');
+
+// Importar cors, para permitir peticiones desde cualquier origen
+const cors = require('cors');
+
+// Importar configuración del proyecto
+const config = require('./backend/config/config');
+
+// Conectar a la base de datos de MongoDB
+require('./backend/config/db');
+
+// Crear el objeto de express
 const app = express();
+
+// Definir el puerto del servidor
 const PORT = config.port;
 
+// Habilitar cors, para permitir peticiones desde cualquier origen
 app.use(cors());
-app.use(express.json());  // Habilitar el parsing de JSON en las peticiones
+
+// Habilitar el parsing de JSON en las peticiones
+app.use(express.json());
 
 // Importar las rutas
 const contactRoutes = require('./backend/routes/contactRoutes');
+
+// Establecer la ruta para las peticiones de contacto
 app.use('/api/contacto', contactRoutes);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
-
 
 
